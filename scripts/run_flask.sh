eef64b463ec9eb086b88560772fb975213de5833
nimish <nimish4july1998@gmail.com>
2020-10-07 01:34:04 +0530

    modified scripts

--- scripts/run_flask.sh
+++ scripts/run_flask.sh
@@ -3,3 +3,5 @@ echo "Write flask app running scripts here";
 # supervisord will run this command "gunicorn app:app --chdir  /home/ubuntu/flaskapp/" in the process in supervisor_for_flask.conf
 
 sudo supervisord -c /home/ubuntu/flaskapp/scripts/supervisor_for_flask.conf 
+
+echo "Ran succesfully supervisor_for_flask.conf"
--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -3,5 +3,7 @@ echo "Write stop server scripts here";
 # kill application running at port 8000
 # sudo lsof -i:8000 -t | xargs -r sudo kill
 
-# Kill process supervisor_for_Flask
+# Kill process supervisor_for_flask
 sudo pkill supervisor_for_flask
+
+echo "killed succesfully";
