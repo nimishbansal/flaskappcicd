1c236ffefc53702129205bf8165e25f073caa496
Ubuntu <ubuntu@ip-172-31-37-94.ec2.internal>
2020-10-06 06:36:26 +0000

    initial commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import time
+app = Flask(__name__)
+@app.route("/")
+def hello():
+    arr = 100000 * [0]
+    time.sleep(1)
+    return "Hello World!"
+
+if __name__ == "__main__":
+    app.run()
+
