86a1f52a92cb14998efdbf9c63ed26c4800a9a57
nimish <nimish4july1998@gmail.com>
2020-10-07 01:26:47 +0530

    testing application stop

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import time
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    return "Hello World! from Argentina"
+    return "Hello World! from Africa"
 
 if __name__ == "__main__":
     app.run()
