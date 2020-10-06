f0ebcd827878660401f681b484ec2caaa3bcbfb2
nimish <nimish4july1998@gmail.com>
2020-10-06 20:47:06 +0530

    testing small change

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 def hello():
     arr = 100000 * [0]
     time.sleep(1)
-    return "Hello World! in India"
+    return "Hello World! in India and America"
 
 if __name__ == "__main__":
     app.run()
