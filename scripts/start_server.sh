2f768449d768dbaa937980e9266182fffe6a8fe7
nimish <nimish4july1998@gmail.com>
2020-10-06 22:45:40 +0530

    added configuration file

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,5 @@
 echo "write start server scripts here";
 
 # Run python script
-bash /home/ubuntu/flaskapp/scripts/run_flask.sh
+# bash /home/ubuntu/flaskapp/scripts/run_flask.sh
+/home/ubuntu/flaskapp/scripts/supervisord
