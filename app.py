3f7abe2549a95ab3cbba4272cc5a9b0716f2687c
nimish <nimish4july1998@gmail.com>
2020-10-07 01:50:19 +0530

    changed script and app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import time
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    return "Hello World! from Africa"
+    return "Hello World! from uganda"
 
 if __name__ == "__main__":
     app.run()
--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -3,7 +3,7 @@ echo "Write stop server scripts here";
 # kill application running at port 8000
 # sudo lsof -i:8000 -t | xargs -r sudo kill
 
-# Kill process supervisor_for_flask
-sudo pkill supervisor_for_flask
+# Kill  gunicorn
+sudo pkill gunicorn
 
 echo "killed succesfully";
